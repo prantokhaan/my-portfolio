e08b815fb8eae6c5d370f1dd1d7529f950d1f06f
prantokhaan <prantokhaan@gmail.com>
2021-09-15 19:20:02 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const aboutMe = () => {
+    console.log('about me clicked');
+}
+const myResume = () => {
+    console.log('resume clicked');
+}
+const myWorks = () => {
+    console.log('works clicked');
+}
+const contactMe = () => {
+    console.log('contact clicked');
+}
